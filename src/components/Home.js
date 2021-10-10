571f38ccb1add23cb6323f4b4721451962c9bd22
Shourya Bansal <shouryab2001@gmail.com>
2021-10-10 16:14:07 +0530

    fourth commit : add rotate wheel functionality

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,11 +10,20 @@ class  Home extends React.Component{
         return (
             <div style={styles.homeScreen}>
                 <div style={styles.menuListLeft}>
+                    <h2>Ipod.js</h2>
                     <ListGroup>
-                        <ListGroup.Item style={{border:'0'}} active>Wallpapers</ListGroup.Item>
-                        <ListGroup.Item style={{border:'0'}}>Music</ListGroup.Item>
-                        <ListGroup.Item style={{border:'0'}}>Games</ListGroup.Item>
-                        <ListGroup.Item style={{border:'0'}}>Settings</ListGroup.Item>
+                    <ListGroup.Item style={{border:'0'}} className={this.props.activeItem==='Wallpapers'?'active':''}>
+                            Wallpapers {this.props.activeItem==='Wallpapers'?<span style={{float:'right' ,fontWeight:'bold'}}>&gt;</span>:''}
+                        </ListGroup.Item>
+                        <ListGroup.Item style={{border:'0'}} className={this.props.activeItem==='Music'?'active':''}>
+                            Music {this.props.activeItem==='Music'?<span style={{float:'right' ,fontWeight:'bold'}}>&gt;</span>:''}
+                        </ListGroup.Item>
+                        <ListGroup.Item style={{border:'0'}} className={this.props.activeItem==='Games'?'active':''}>
+                            Games {this.props.activeItem==='Games'?<span style={{float:'right' ,fontWeight:'bold'}}>&gt;</span>:''}
+                        </ListGroup.Item>
+                        <ListGroup.Item style={{border:'0'}} className={this.props.activeItem==='Settings'?'active':''}>
+                            Settings {this.props.activeItem==='Settings'?<span style={{float:'right' ,fontWeight:'bold'}}>&gt;</span>:''}
+                        </ListGroup.Item>
                     </ListGroup>
                 </div>
                 <div style={styles.imageRight}></div>
--- src/components/Screen.js
+++ src/components/Screen.js
@@ -9,7 +9,7 @@ class Screen extends React.Component{
     render() {
         return (
           <div style={style.screen}>
-            <Home />
+            <Home activeItem={this.props.activeItem}/>
           </div>
         );
     }  
